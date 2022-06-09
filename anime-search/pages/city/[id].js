6c22ecb8cd6b16d78fd975f675b3a70b47c5c03d
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-06-09 16:03:49 +1000

    testing dynamic page bug

--- anime-search/pages/city/[id].js
+++ anime-search/pages/city/[id].js
@@ -8,14 +8,15 @@ function City() {
     const router = useRouter();
     const { id } = router.query;
 
-    const [cityData, setCityData] = useState();
-    const [weatherIcon, setWeatherIcon] = useState();
+    const [cityData, setCityData] = useState('');
+    const [weatherIcon, setWeatherIcon] = useState('');
+    const [temperature, setTemperature] = useState('');
 
     
     const fetchData = async () => {
         
         // const api = process.env.API_KEY3;
-        const api = 'd515c35362237a42c19d6d5d1aadbf97';
+        const api = 'c0c1f25e4565ff104b77d4feba3777a6';
         
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${id},Au&units=metric&APPID=${api}`;
         
@@ -24,14 +25,15 @@ function City() {
 
         setCityData(data)
         setWeatherIcon(data?.weather[0].icon)
+        setTemperature(data?.main.temp)
     }
 
 
     useEffect(() => {
         fetchData()
-    });
+    }, []);
 
-    console.log(cityData);
+    console.log(temperature);
 
     
     return ( 
@@ -39,15 +41,15 @@ function City() {
             <Header title={id} />
 
             <h1 className='capitalize font-semibold text-2xl'>{id}</h1>
-            <h1>Current temperature { Math.round(cityData?.main.temp) } C</h1>
-            <h1>{ cityData?.weather[0].description }</h1>
+            <h1>Current temperature { Math.round(temperature) } C</h1>
+            {/* <h1>{ cityData?.weather[0].description }</h1> */}
 
-        <WeatherSVG 
+        {/* <WeatherSVG 
             weatherIcon={weatherIcon} 
             cityData={cityData}
             height={100}
             width={100}
-            />
+            /> */}
             
 
         </div>
