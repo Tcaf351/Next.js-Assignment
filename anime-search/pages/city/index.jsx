878364656cdc27e21694bc7d33457d26c0714930
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-06-16 11:36:51 +1000

    fixed layout on buttons page

--- anime-search/pages/city/index.jsx
+++ anime-search/pages/city/index.jsx
@@ -4,34 +4,41 @@ import Header from '../../components/Header';
 function Search() {
 
     return ( 
-        <div className="min-h-screen bg-gradient-to-bl from-orange-200 via-cyan-300 to-sky-300">
+        <div className="min-h-screen bg-gradient-to-tr from-gray-200 to-gray-300">
             <Header title="Search" />
 
-            <div className='flex flex-col items-center justify-center h-1/2 pt-6'>
-                <Link href="/city/melbourne">
-                    <a>Melbourne</a>
-                </Link>
-                <Link href="/city/sydney">
-                    <a>Sydney</a>
-                </Link>
-                <Link href="/city/brisbane">
-                    <a>Brisbane</a>
-                </Link>
-                <Link href="/city/canberra">
-                    <a>Canberra</a>
-                </Link>
-                <Link href="/city/darwin">
-                    <a>Darwin</a>
-                </Link>
-                <Link href="/city/adelaide">
-                    <a>Adelaide</a>
-                </Link>
-                <Link href="/city/hobart">
-                    <a>Hobart</a>
-                </Link>
-                <Link href="/city/perth">
-                    <a>Perth</a>
-                </Link>
+            <div className='md:flex md:flex-col md:items-center md:justify-center h-screen w-screen pt-6'>
+                <div className='flex flex-row items-center justify-center w-1/2'>
+                    <Link href="/city/melbourne">
+                        <a className='border-2 border-sky-400 hover:border-orange-400 md:px-7 md:py-2 rounded-lg mx-14 my-10'>Melbourne</a>
+                    </Link>
+                    <Link href="/city/sydney">
+                        <a className='border-2 border-sky-400 hover:border-orange-400 md:px-7 md:py-2 rounded-lg mx-14 my-10'>Sydney</a>
+                    </Link>
+                    <Link href="/city/brisbane">
+                        <a className='border-2 border-sky-400 hover:border-orange-400 md:px-7 md:py-2 rounded-lg mx-14 my-10'>Brisbane</a>
+                    </Link>
+                </div>
+                
+                <div className='flex flex-row items-center justify-center'>
+                    <Link href="/city/canberra">
+                        <a className='border-2 border-sky-400 hover:border-orange-400 md:px-7 md:py-2 rounded-lg mx-14 my-10'>Canberra</a>
+                    </Link>
+                    <Link href="/city/darwin">
+                        <a className='border-2 border-sky-400 hover:border-orange-400 md:px-7 md:py-2 rounded-lg mx-14 my-10'>Darwin</a>
+                    </Link>
+                    <Link href="/city/adelaide">
+                        <a className='border-2 border-sky-400 hover:border-orange-400 md:px-7 md:py-2 rounded-lg mx-14 my-10'>Adelaide</a>
+                    </Link>
+                </div>
+                <div className='flex flex-row items-center justify-center'>
+                    <Link href="/city/hobart">
+                        <a className='border-2 border-sky-400 hover:border-orange-400 md:px-7 md:py-2 rounded-lg mx-14 my-10'>Hobart</a>
+                    </Link>
+                    <Link href="/city/perth">
+                        <a className='border-2 border-sky-400 hover:border-orange-400 md:px-7 md:py-2 rounded-lg mx-14 my-10'>Perth</a>
+                    </Link>
+                </div>
             </div>
         </div>
      );
